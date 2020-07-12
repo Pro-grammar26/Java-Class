17f1bcf48474e3e1cd9b24e27f9fc115676d7c50
Sahith Chandra <44122904+Pro-grammar26@users.noreply.github.com>
2020-07-12 15:11:12 -0700

    My If And Else Programs

---
+++ ForLoops/src/ForLoopsClasswork.java
@@ -0,0 +1,13 @@
+
+public class ForLoopsClasswork {
+
+	public static void main(String[] args) {
+		int x = 0;
+		for (x = 0; x <= 100; x = x + 10) {
+
+			System.out.println(x);
+		}
+
+	}
+
+}
---
+++ ForLoops/src/ForLoopsHomework.java
@@ -0,0 +1,18 @@
+
+public class ForLoopsHomework {
+
+	public static void main(String[] args) {
+		
+		int number = 13;
+		
+			
+		if(number % 2 == 0) {
+		
+			System.out.println("The Number " + number + " is even");			
+		}else {
+			System.out.println("The number " + number + " is odd");
+		}
+	
+	}
+	
+}
