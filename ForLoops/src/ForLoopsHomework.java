cd3e16557200839470f602ca84660d22c144a745
Sahith Chandra <44122904+Pro-grammar26@users.noreply.github.com>
2020-07-12 18:44:18 -0700

    My For Loop Programs

---
+++ ForLoops/src/ForLoopsHomework.java
@@ -0,0 +1,16 @@
+
+public class ForLoopsHomework {
+
+	public static void main(String[] args) {
+		
+		for(int x = 0; x<=10 ; x++) {
+		
+		int tableNumber = 6;	
+		
+		System.out.println(tableNumber + " * " + x + " = " + tableNumber*x);
+		
+		}
+		
+	}
+
+}
