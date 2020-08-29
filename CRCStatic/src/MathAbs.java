e855ce4aa981e2816487ed231a26af284427cb00
Sahith Chandra <44122904+Pro-grammar26@users.noreply.github.com>
2020-08-29 12:47:55 -0700

    Abs and Sqrt Practice

---
+++ CRCStatic/src/MathAbs.java
@@ -0,0 +1,12 @@
+
+public class MathAbs {
+
+	public static void main(String[] args) {
+
+		int Answer = Math.abs(-10);
+
+		System.out.println(Answer);
+
+	}
+
+}
---
+++ CRCStatic/src/MathSqrt.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class MathSqrt {
+
+	public static void main(String[] args) {
+
+		double Answer = Math.sqrt(3);
+
+		System.out.println(Answer);
+
+	}
+
+}
